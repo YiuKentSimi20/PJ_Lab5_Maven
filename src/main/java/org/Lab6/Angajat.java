46f6afd9e5d8bb7c06d495ea624dcd91987d8d42
YiuKentSimi20 <alexandru.ilia@student.upt.ro>
2024-11-05 13:05:01 +0200

    Tema 6

--- src/main/java/org/Lab6/Angajat.java
+++ src/main/java/org/Lab6/Angajat.java
@@ -2,14 +2,28 @@ package org.Lab6;
 
 import java.time.LocalDate;
 
+/**
+ * Clasa Angajat - reprezinta angajatii dintr-o companie
+ * @author ILIA Alexandru
+ */
 public class Angajat {
     private String nume;
     private String post;
     private LocalDate data_angajarii;
     private float salar;
 
+    /**
+     * Constructor fara parametrii
+     */
     public Angajat() {}
 
+    /**
+     * Constructor cu parametrii
+     * @param nume numele angajatului
+     * @param post postul ocupat
+     * @param data_angajarii data angajarii
+     * @param salar salar in RON
+     */
     public Angajat(String nume, String post, LocalDate data_angajarii, float salar) {
         this.nume = nume;
         this.post = post;
@@ -57,18 +71,34 @@ public class Angajat {
         return -1;
     }
 
+    /**
+     * Returneaza numele cu majuscule pentru lucrul cu stream-uri
+     * @return
+     */
     public String numeToUpperCase(){
         return nume.toUpperCase();
     }
 
+    /**
+     * Se apeleaza prin referinta atunci cand obiectul din clasa Optional, dupa prelucrarea cu Stream API, NU este gol
+     * si returneaza un mesaj corespunzator
+     */
     public void angajat_Ion_found(){
         System.out.println("Firma are cel putin un Ion angajat");
     }
 
+    /**
+     * Se apeleaza prin referinta atunci cand obiectul din clasa Optional, dupa prelucrarea cu Stream API, ESTE gol
+     * si returneaza un mesaj corespunzator
+     */
     public static void angajat_Ion_not_found(){
         System.out.println("Firma nu are niciun Ion angajat");
     }
 
+    /**
+     * Override la functia toString()
+     * @return returneaza elementele separate prin virgula
+     */
     @Override
     public String toString() {
         return nume + "," + post + "," + data_angajarii + "," + salar;
